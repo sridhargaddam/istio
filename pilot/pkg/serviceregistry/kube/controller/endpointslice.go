b259e9435566419976d33e83d53338a01873c8f1
Sridhar Gaddam <sgaddam@redhat.com>
2025-12-16 18:33:57 +0530

    Support for User Defined Networks with Sail Operator
    This PR modifies the Istio code to listen on mirrored endpoint slices
    instead of the regular k8s endpoint slices using some feature flag.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pilot/pkg/serviceregistry/kube/controller/endpointslice.go
+++ pilot/pkg/serviceregistry/kube/controller/endpointslice.go
@@ -30,6 +30,7 @@ import (
 	"istio.io/api/annotation"
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/serviceregistry/kube/endpointslice"
 	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/config/schema/kind"
@@ -179,7 +180,8 @@ func (esc *endpointSliceController) GetProxyServiceTargets(proxy *model.Proxy) [
 }
 
 func serviceNameForEndpointSlice(labels map[string]string) string {
-	return labels[v1.LabelServiceName]
+	name, _ := endpointslice.GetServiceNameFromLabels(labels)
+	return name
 }
 
 func (esc *endpointSliceController) serviceTargets(ep *v1.EndpointSlice, proxy *model.Proxy) []model.ServiceTarget {
@@ -437,8 +439,9 @@ func (e *endpointSliceCache) has(hostname host.Name) bool {
 }
 
 func endpointSliceSelectorForService(name string) klabels.Selector {
+	labelKey := endpointslice.GetServiceLabelKey()
 	return klabels.Set(map[string]string{
-		v1.LabelServiceName: name,
+		labelKey: name,
 	}).AsSelectorPreValidated().Add(*endpointSliceRequirement)
 }
 
