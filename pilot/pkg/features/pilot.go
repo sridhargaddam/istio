384688245a3bbe27f860707c53a574d0532f6f43
Sridhar Gaddam <sgaddam@redhat.com>
2025-12-16 18:33:57 +0530

    Support for User Defined Networks with Sail Operator
    This PR modifies the Istio code to listen on mirrored endpoint slices
    instead of the regular k8s endpoint slices using some feature flag.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -296,7 +296,7 @@ var (
 	// See https://github.com/istio/istio/issues/56007 for more details of this feature with examples.
 	EnableAbsoluteFqdnVhostDomain = env.Register(
 		"PILOT_ENABLE_ABSOLUTE_FQDN_VHOST_DOMAIN", // Environment variable name
-		true, // Default value (true = feature enabled by default)
+		true,                                      // Default value (true = feature enabled by default)
 		"If set to false, Istio will not add the absolute FQDN variant"+
 			" (e.g., my-service.my-ns.svc.cluster.local.) to the domains"+
 			" list for VirtualHost entries.",
@@ -339,6 +339,14 @@ var (
 
 	DisableTrackRemainingMetrics = env.Register("DISABLE_TRACK_REMAINING_CB_METRICS", true,
 		"If disabled, the remaining metrics for circuit breakers will not be tracked.").Get()
+
+	EnableOVNKubernetesUDN = env.Register(
+		"PILOT_ENABLE_OVNK_UDN",
+		true,
+		"If enabled, Istio will watch OVN-Kubernetes UDN mirrored EndpointSlices with label 'k8s.ovn.org/service-name'. "+
+			"This enables Istio to provide service connectivity within User-Defined Networks (UDN) on OpenShift. "+
+			"Requires platform to be set to 'openshift'.",
+	).Get()
 
 	BlockedCIDRsInJWKURIs = func() []*net.IPNet {
 		v := env.Register(
--- pilot/pkg/serviceregistry/kube/controller/ambient/workloads.go
+++ pilot/pkg/serviceregistry/kube/controller/ambient/workloads.go
@@ -34,6 +34,8 @@ import (
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/serviceregistry/kube"
+	"istio.io/istio/pilot/pkg/serviceregistry/kube/controller/ambient/multicluster"
+	"istio.io/istio/pilot/pkg/serviceregistry/kube/endpointslice"
 	"istio.io/istio/pilot/pkg/serviceregistry/serviceentry"
 	labelutil "istio.io/istio/pilot/pkg/serviceregistry/util/label"
 	"istio.io/istio/pilot/pkg/util/protoconv"
@@ -832,7 +834,7 @@ func matchingServicesWithoutSelectors(
 	// For each IP, find any endpointSlices referencing it.
 	matchedSlices := endpointSlicesAddressIndex.Fetch(ctx, tr)
 	for _, es := range matchedSlices {
-		serviceName, f := es.Labels[discovery.LabelServiceName]
+		serviceName, f := endpointslice.GetServiceNameFromLabels(es.Labels)
 		if !f {
 			// Not for a service; we don't care about it.
 			continue
@@ -1048,7 +1050,7 @@ func endpointSlicesBuilder(
 		// We only care about EndpointSlices that are for a Service.
 		// Otherwise, it is just an arbitrary bag of IP addresses for some user-specific purpose, which doesn't have a clear
 		// usage for us (if it had some additional info like service account, etc, then perhaps it would be useful).
-		serviceName, f := es.Labels[discovery.LabelServiceName]
+		serviceName, f := endpointslice.GetServiceNameFromLabels(es.Labels)
 		if !f {
 			return nil
 		}
@@ -1483,7 +1485,7 @@ func endpointSliceAddressIndex(EndpointSlices krt.Collection[*discovery.Endpoint
 			// Currently we do not support FQDN.
 			return nil
 		}
-		_, f := es.Labels[discovery.LabelServiceName]
+		_, f := endpointslice.GetServiceNameFromLabels(es.Labels)
 		if !f {
 			// Not for a service; we don't care about it.
 			return nil
